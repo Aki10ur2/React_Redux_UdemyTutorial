4a74602117eef398b9db19a2ea2ac209b9d3280e
aki10ur2 <default0606@gmail.com>
2020-02-02 15:06:36 +0900

    JSXのreturnがノードをひとつしか返せない制約上、無駄なノード(divなど)描画したくない時にReact.Frangmentでまとめる方法

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          Hello, world!
-        </h1>
-      </div>
+      <React.Fragment>
+        <label htmlFor="huga"></label>
+        <input type="text" onChange={() => {console.log('text changed')}}/>
+      </React.Fragment>
     );
   }
 }
