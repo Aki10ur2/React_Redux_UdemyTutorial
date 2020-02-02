c99d6688879e07be078b88fe38d67f3da26d9b2f
aki10ur2 <default0606@gmail.com>
2020-02-02 17:45:01 +0900

    add package of redux and react-redux to create action and actioncreater

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+/** このオブジェクト（index.js）はAction Creater とよばれる。 */
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+export const increment = () => ({
+    type: INCREMENT
+  })
+
+export const decrement = () =>({
+  type: DECREMENT
+})
