16789fdac5840b0bd0a143b9b751b8f84914e7d3
aki10ur2 <default0606@gmail.com>
2020-02-02 15:14:35 +0900

    create class components and functional components example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="huga"></label>
-        <input type="text" onChange={() => {console.log('text changed')}}/>
-      </React.Fragment>
-    );
-  }
-}
+/**
+ * Class Component の例
+ */
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="huga"></label>
+//         <input type="text" onChange={() => {console.log('text changed')}}/>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+/**
+ * Functional Component の例
+ */
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
