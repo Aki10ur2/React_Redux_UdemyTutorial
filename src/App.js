d6815d540fa2a7a754278d08f7f6b6be83c71bbf
aki10ur2 <default0606@gmail.com>
2020-02-02 11:21:48 +0900

    jsx tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello, world!</h1></div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello, world!
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
